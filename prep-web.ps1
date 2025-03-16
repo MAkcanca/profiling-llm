2e9a96f13c1fc966078b6d885b3ceb2d0a1130b4
magpi <mustafa.the.science.guy@gmail.com>
2025-03-16 23:07:03 +0300

    update web

--- prep-web.ps1
+++
@@ -1,9 +0,0 @@
-# Powershell script that will copy generated_profiles/ and gold-standards/ into the web directory while removing the previously existing files
-
-# Remove existing files
-Remove-Item -Recurse -Force web-dashboard/generated_profiles/*
-Remove-Item -Recurse -Force web-dashboard/gold-standards/*
-
-# Copy new files
-Copy-Item -Recurse -Force generated_profiles web-dashboard/
-Copy-Item -Recurse -Force gold-standards web-dashboard/
--- prep-web.sh
+++
@@ -1,9 +0,0 @@
-# Bash script that will copy generated_profiles/ and gold-standards/ into the web directory while removing the previously existing files
-
-# Remove existing files
-rm -rf web-dashboard/generated_profiles/*
-rm -rf web-dashboard/gold-standards/*
-
-# Copy new files
-cp -r generated_profiles web-dashboard/
-cp -r gold-standards web-dashboard/
